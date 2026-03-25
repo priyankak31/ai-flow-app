5aab21d06b205a1b2eed178020b0fcb5147671d5
Priyanka31 <priyankakumavat10@gmail.com>
2026-03-25 14:57:34 +0530

    added server url

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,12 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",                        
+    "https://ai-flow-app-frontend2.onrender.com/"         
+  ]
+}));
 app.use(express.json());
 mongoose
   .connect(process.env.MONGODB_URI)
